138d3a95721d2486121b33f67b5b116ad6b2cc87
LiviaJonnatan <77211770+LiviaJonnatan@users.noreply.github.com>
2022-03-10 11:33:59 -0800

    Update LegalMove.java

--- team-00/src/main/java/ubc/cosc322/LegalMove.java
+++ team-00/src/main/java/ubc/cosc322/LegalMove.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class LegalMove {
 	
-	public ArrayList<Position> getLegalMove (Queen queen, Board[][] board) {
+	public ArrayList<Position> getLegalMove (Queen queen, Board board) {
 		ArrayList<Position> moves = new ArrayList<>();
 
 		int currentRow = queen.getQueenCurrX();
@@ -12,56 +12,56 @@ public class LegalMove {
 		
 		// Right
 		for (int i = 1; currentCol+i < 10; i++) {
-			if(board[currentRow][currentCol+i] == null) {
+			if(board.getBoardPos(currentRow, currentCol+i) == 0) {
 				moves.add(new Position(currentRow, currentCol+i));
 			}
 		}
 		
 		// Left
 		for (int i = 1; currentCol-i >= 0; i++) {
-			if(board[currentRow][currentCol-i] == null) {
+			if(board.getBoardPos(currentRow, currentCol-i) == 0) {
 				moves.add(new Position(currentRow, currentCol-i));
 			}
 		}
 		
 		// Up
 		for (int i = 1; currentRow-i >= 0; i++) {
-			if(board[currentRow-i][currentCol] == null) {
+			if(board.getBoardPos(currentRow-i, currentCol) == 0) {
 				moves.add(new Position(currentRow-i, currentCol));
 			}
 		}
 		
 		// Down
 		for (int i = 1; currentRow+i < 10; i++) {
-			if(board[currentRow+i][currentCol] == null) {
+			if(board.getBoardPos(currentRow+i, currentCol) == 0) {
 				moves.add(new Position(currentRow+i, currentCol));
 			}
 		}
 		
 		// Up-left
 		for (int i = 1; currentRow-i >= 0 && currentCol-i >= 0; i++) {
-			if(board[currentRow-i][currentCol-i] == null) {
+			if(board.getBoardPos(currentRow-i, currentCol-i) == 0) {
 				moves.add(new Position(currentRow-i, currentCol-i));
 			}
 		}
 		
 		// Up-right
 		for (int i = 1; currentRow-i >= 0 && currentCol+i < 10; i++) {
-			if(board[currentRow-i][currentCol+i] == null) {
+			if(board.getBoardPos(currentRow-i, currentCol+i) == 0) {
 				moves.add(new Position(currentRow-i, currentCol+i));
 			}
 		}
 		
 		// Down-right
 		for (int i = 1; currentRow+i < 10 && currentCol+i < 10; i++) {
-			if(board[currentRow+i][currentCol+i] == null) {
+			if(board.getBoardPos(currentRow+i, currentCol+i) == 0) {
 				moves.add(new Position(currentRow+i, currentCol+i));
 			}
 		}
 		
 		// Down-left
 		for (int i = 1; currentRow+i < 10 && currentCol-i >= 0; i++) {
-			if(board[currentRow+i][currentCol-i] == null) {
+			if(board.getBoardPos(currentRow+i, currentCol-i) == 0) {
 				moves.add(new Position(currentRow+i, currentCol-i));
 			}
 		}
