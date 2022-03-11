6e8713b49512fda1d1ff9b6f24c0960f4fb320e0
Danyulll <dkrasnov@student.ubc.ca>
2022-03-10 19:33:15 -0800

    Full support for black and white queen game tree

--- team-00/src/main/java/ubc/cosc322/COSC322Test.java
+++ team-00/src/main/java/ubc/cosc322/COSC322Test.java
@@ -111,49 +111,44 @@ public class COSC322Test extends GamePlayer {
 			boolean white = (this.whiteUser.equals(this.userName)) ? true : false;
 
 			Board board = new Board();
-			
+
 			// If we are player one make a move
 			if (white) {
 				System.out.println("I am player one (white)");
 				Board boardBeforeMove = (Board) board.clone();
-				//Construct initial Game tree
+				// Construct initial Game tree
 				System.out.println("Constructing tree");
 				Tree partial = new Tree();
 				partial = partial.generatePartialGameTree(board, white);
 				System.out.println("Selecting Board");
-				
-				//Make move decision
-				
+				// Make move decision
 				System.out.println("original board:");
 				boardBeforeMove.printBoard();
-				
-				Board moveToMake = partial.getRoot().getChildren().get((int)(Math.random()*2468)).getBoard();
+				Board moveToMake = partial.getRoot().getChildren().get((int) (Math.random() * 2468)).getBoard();
 				System.out.println("printing board move chosen");
 				moveToMake.printBoard();
 				TerritoryHeuristic heur = new TerritoryHeuristic();
 				heur.printHeuristic(heur.closestQueen(board));
-				//TODO turn this chosen board into coords that I can send to the server
-				//Get Move Coords
-				int[] oldWhiteQueenCoord = new int[2]; 
-				int[] newWhiteQueenCoord = new int[2];			
+				// Get Move Coords
+				int[] oldWhiteQueenCoord = new int[2];
+				int[] newWhiteQueenCoord = new int[2];
 				int[] newArrowcoord = new int[2];
-				
+
 				for (int i = 0; i < boardBeforeMove.board.length; i++) {
 					for (int j = 0; j < boardBeforeMove.board.length; j++) {
-						if(moveToMake.board[i][j] != boardBeforeMove.board[i][j]) {
-							if(moveToMake.board[i][j]==3) {
+						if (moveToMake.board[i][j] != boardBeforeMove.board[i][j]) {
+							if (moveToMake.board[i][j] == 3 && boardBeforeMove.board[i][j] != 3) {
 								newArrowcoord[0] = i;
 								newArrowcoord[1] = j;
-							}
-							else if(boardBeforeMove.board[i][j] ==0 && moveToMake.board[i][j]==1) {
+							} else if (boardBeforeMove.board[i][j] == 0 && moveToMake.board[i][j] == 1) {
 								newWhiteQueenCoord[0] = i;
 								newWhiteQueenCoord[1] = j;
 							}
-						
-						if(moveToMake.board[i][j]==0 && boardBeforeMove.board[i][j]==1) {
-							oldWhiteQueenCoord[0] = i;
-							oldWhiteQueenCoord[1] = j;
-						}
+
+							if (moveToMake.board[i][j] == 0 && boardBeforeMove.board[i][j] == 1) {
+								oldWhiteQueenCoord[0] = i;
+								oldWhiteQueenCoord[1] = j;
+							}
 						}
 					}
 				}
@@ -166,79 +161,126 @@ public class COSC322Test extends GamePlayer {
 				ArrayList<Integer> QueenOld = new ArrayList<>();
 				QueenOld.add(oldWhiteQueenCoord[0]);
 				QueenOld.add(oldWhiteQueenCoord[1]);
-				//System.out.println("New ArrowCoord = " + newArrowcoord[0] + ","+newArrowcoord[1]);
-				//System.out.println("New QueenCoord = " + newWhiteQueenCoord[0] + ","+newWhiteQueenCoord[1]);
-				//Convert To sendable coords
-				HashMap<ArrayList<Integer>,ArrayList<Integer>> gaoTable = Board.makeGaoTable();
-				
-				//Send Move
+				// Convert To sendable coords
+				HashMap<ArrayList<Integer>, ArrayList<Integer>> gaoTable = Board.makeGaoTable();
+				// Send Move
 				ArrayList<Integer> QueenPosCurSend = new ArrayList<>();
 				QueenPosCurSend = gaoTable.get(QueenOld);
-				
 				ArrayList<Integer> QueenPosNewSend = new ArrayList<>();
 				QueenPosNewSend = gaoTable.get(QueenNew);
-				
 				ArrayList<Integer> ArrowPosSend = new ArrayList<>();
 				ArrowPosSend = gaoTable.get(Arrow);
-				
-				
-				
-				
 				gameClient.sendMoveMessage(QueenPosCurSend, QueenPosNewSend, ArrowPosSend);
 				this.gamegui.updateGameState(QueenPosCurSend, QueenPosNewSend, ArrowPosSend);
 			} else if (!white) { // If we are player 2 wait to recieve a move and then make a move
 				System.out.println("I am player 2 (black)");
-				
-				//wait to recieve move
-				
-				//construct game tree
-				
-				//make move decision
-				
-				//send move
+
+				// wait to receive move
+				ArrayList<Integer> QueenPosCurEnemey = (ArrayList<Integer>) msgDetails
+						.get(AmazonsGameMessage.QUEEN_POS_CURR);
+				ArrayList<Integer> QueenPosNewEnemey = (ArrayList<Integer>) msgDetails
+						.get(AmazonsGameMessage.Queen_POS_NEXT);
+				ArrayList<Integer> ArrowPosEnemey = (ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.ARROW_POS);
+				//update board
+				board.updateGameBoard(board, QueenPosCurEnemey, QueenPosNewEnemey, ArrowPosEnemey, white);
+				Board boardBeforeMove = (Board) board.clone();
+				// Construct initial Game tree
+				System.out.println("Constructing tree");
+				Tree partial = new Tree();
+				partial = partial.generatePartialGameTree(board, white);
+				System.out.println("Selecting Board");
+				// Make move decision
+				System.out.println("original board:");
+				boardBeforeMove.printBoard();
+				Board moveToMake = partial.getRoot().getChildren().get((int) (Math.random() * 2468)).getBoard();
+				System.out.println("printing board move chosen");
+				moveToMake.printBoard();
+				TerritoryHeuristic heur = new TerritoryHeuristic();
+				heur.printHeuristic(heur.closestQueen(board));
+				// Get Move Coords
+				int[] oldBlackQueenCoord = new int[2];
+				int[] newBlackQueenCoord = new int[2];
+				int[] newArrowcoord = new int[2];
+
+				for (int i = 0; i < boardBeforeMove.board.length; i++) {
+					for (int j = 0; j < boardBeforeMove.board.length; j++) {
+						if (moveToMake.board[i][j] != boardBeforeMove.board[i][j]) {
+							if (moveToMake.board[i][j] == 3 && boardBeforeMove.board[i][j] != 3) {
+								newArrowcoord[0] = i;
+								newArrowcoord[1] = j;
+							} else if (boardBeforeMove.board[i][j] == 0 && moveToMake.board[i][j] == 2) {
+								newBlackQueenCoord[0] = i;
+								newBlackQueenCoord[1] = j;
+							}
+
+							if (moveToMake.board[i][j] == 0 && boardBeforeMove.board[i][j] == 2) {
+								oldBlackQueenCoord[0] = i;
+								oldBlackQueenCoord[1] = j;
+							}
+						}
+					}
+				}
+				ArrayList<Integer> QueenNew = new ArrayList<Integer>();
+				QueenNew.add(newBlackQueenCoord[0]);
+				QueenNew.add(newBlackQueenCoord[1]);
+				ArrayList<Integer> Arrow = new ArrayList<Integer>();
+				Arrow.add(newArrowcoord[0]);
+				Arrow.add(newArrowcoord[1]);
+				ArrayList<Integer> QueenOld = new ArrayList<>();
+				QueenOld.add(oldBlackQueenCoord[0]);
+				QueenOld.add(oldBlackQueenCoord[1]);
+				// Convert To sendable coords
+				HashMap<ArrayList<Integer>, ArrayList<Integer>> gaoTable = Board.makeGaoTable();
+				// Send Move
+				ArrayList<Integer> QueenPosCurSend = new ArrayList<>();
+				QueenPosCurSend = gaoTable.get(QueenOld);
+				ArrayList<Integer> QueenPosNewSend = new ArrayList<>();
+				QueenPosNewSend = gaoTable.get(QueenNew);
+				ArrayList<Integer> ArrowPosSend = new ArrayList<>();
+				ArrowPosSend = gaoTable.get(Arrow);
+				gameClient.sendMoveMessage(QueenPosCurSend, QueenPosNewSend, ArrowPosSend);
+				this.gamegui.updateGameState(QueenPosCurSend, QueenPosNewSend, ArrowPosSend);
 			}
 
-			/*ArrayList<Integer> QueenPosCurEnemey = (ArrayList<Integer>) msgDetails
-					.get(AmazonsGameMessage.QUEEN_POS_CURR);
-			ArrayList<Integer> QueenPosNewEnemey = (ArrayList<Integer>) msgDetails
-					.get(AmazonsGameMessage.Queen_POS_NEXT);
-			ArrayList<Integer> ArrowPosEnemey = (ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.ARROW_POS);
-
-			//board.printBoard();
-
-			// Calculate Heuristic and print
-			TerritoryHeuristic heur = new TerritoryHeuristic();
-			TerritoryHeuristic.printHeuristic(heur.closestQueen(board));
-			// Update game board
-			board.updateGameBoard(board, QueenPosCurEnemey, QueenPosNewEnemey, ArrowPosEnemey,true);
-
-			// Our positions to send
-
-			ArrayList<Integer> QueenPosCurSend = new ArrayList<>();
-			ArrayList<Integer> QueenPosNewSend = new ArrayList<>();
-			ArrayList<Integer> ArrowPosSend = new ArrayList<>();
-
-			QueenPosCurSend.add(4); // row [4,1]
-			QueenPosCurSend.add(1); // col
-
-			QueenPosNewSend.add(5);
-			QueenPosNewSend.add(2); // [5,2]
-
-			ArrowPosSend.add(5);
-			ArrowPosSend.add(3);
-
-			// print the board after all the moves have been made
-			board.updateGameBoard(board, QueenPosCurSend, QueenPosNewSend, ArrowPosSend,true);
-			board.printBoard();
-
-			// Calculate Heuristic and print
-			TerritoryHeuristic.printHeuristic(heur.closestQueen(board));
-
-			// Sending the positions
-			gameClient.sendMoveMessage(QueenPosCurSend, QueenPosNewSend, ArrowPosSend);
-
-			this.gamegui.updateGameState(QueenPosCurSend, QueenPosNewSend, ArrowPosSend);
-			*/t = 30;
+			/*
+			 * ArrayList<Integer> QueenPosCurEnemey = (ArrayList<Integer>) msgDetails
+			 * .get(AmazonsGameMessage.QUEEN_POS_CURR); ArrayList<Integer> QueenPosNewEnemey
+			 * = (ArrayList<Integer>) msgDetails .get(AmazonsGameMessage.Queen_POS_NEXT);
+			 * ArrayList<Integer> ArrowPosEnemey = (ArrayList<Integer>)
+			 * msgDetails.get(AmazonsGameMessage.ARROW_POS);
+			 * 
+			 * //board.printBoard();
+			 * 
+			 * // Calculate Heuristic and print TerritoryHeuristic heur = new
+			 * TerritoryHeuristic();
+			 * TerritoryHeuristic.printHeuristic(heur.closestQueen(board)); // Update game
+			 * board board.updateGameBoard(board, QueenPosCurEnemey, QueenPosNewEnemey,
+			 * ArrowPosEnemey,true);
+			 * 
+			 * // Our positions to send
+			 * 
+			 * ArrayList<Integer> QueenPosCurSend = new ArrayList<>(); ArrayList<Integer>
+			 * QueenPosNewSend = new ArrayList<>(); ArrayList<Integer> ArrowPosSend = new
+			 * ArrayList<>();
+			 * 
+			 * QueenPosCurSend.add(4); // row [4,1] QueenPosCurSend.add(1); // col
+			 * 
+			 * QueenPosNewSend.add(5); QueenPosNewSend.add(2); // [5,2]
+			 * 
+			 * ArrowPosSend.add(5); ArrowPosSend.add(3);
+			 * 
+			 * // print the board after all the moves have been made
+			 * board.updateGameBoard(board, QueenPosCurSend, QueenPosNewSend,
+			 * ArrowPosSend,true); board.printBoard();
+			 * 
+			 * // Calculate Heuristic and print
+			 * TerritoryHeuristic.printHeuristic(heur.closestQueen(board));
+			 * 
+			 * // Sending the positions gameClient.sendMoveMessage(QueenPosCurSend,
+			 * QueenPosNewSend, ArrowPosSend);
+			 * 
+			 * this.gamegui.updateGameState(QueenPosCurSend, QueenPosNewSend, ArrowPosSend);
+			 */t = 30;
 			Timer timer = new Timer();
 			timer.schedule(new countDown(), 0, 5000);
 			break;
--- team-00/src/main/java/ubc/cosc322/Tree.java
+++ team-00/src/main/java/ubc/cosc322/Tree.java
@@ -49,13 +49,14 @@ public class Tree {
 			for (int i = 0; i < 4; i++) {
 				Queen Queen = new Queen(new Position(whiteQueenLocs.get(i)[0], whiteQueenLocs.get(i)[1]));
 				/*
-				System.out.println("Queen1: " + whiteQueenLocs.get(0)[0] + "," + whiteQueenLocs.get(0)[1] + "\n"
-						+ "Queen2: " + whiteQueenLocs.get(1)[0] + "," + whiteQueenLocs.get(1)[1] + "\n" + "Queen3: "
-						+ whiteQueenLocs.get(2)[0] + "," + whiteQueenLocs.get(2)[1] + "\n" + "Queen4: "
-						+ whiteQueenLocs.get(3)[0] + "," + whiteQueenLocs.get(3)[1] + "\n");*/
+				 * System.out.println("Queen1: " + whiteQueenLocs.get(0)[0] + "," +
+				 * whiteQueenLocs.get(0)[1] + "\n" + "Queen2: " + whiteQueenLocs.get(1)[0] + ","
+				 * + whiteQueenLocs.get(1)[1] + "\n" + "Queen3: " + whiteQueenLocs.get(2)[0] +
+				 * "," + whiteQueenLocs.get(2)[1] + "\n" + "Queen4: " + whiteQueenLocs.get(3)[0]
+				 * + "," + whiteQueenLocs.get(3)[1] + "\n");
+				 */
 				LegalMove moveGetter = new LegalMove();
 				ArrayList<Position> movesQueen = moveGetter.getLegalMove(Queen, curr);
-
 				for (Position position : movesQueen) {
 					ArrayList<Position> arrowMoves = moveGetter.getLegalMove(new Queen(position), curr);
 					for (Position position2 : arrowMoves) {
@@ -87,116 +88,63 @@ public class Tree {
 
 			}
 
-			/*
-			 * Queen Queen1 = new Queen(new Position(whiteQueenLocs.get(0)[0],
-			 * whiteQueenLocs.get(0)[1])); System.out.println("Positiion of Queen 1: " +
-			 * Queen1.getQueenCurrX() + "," + Queen1.getQueenCurrY()); // Queen Queen2 = new
-			 * Queen(new Position(whiteQueenLocs.get(1)[0], // whiteQueenLocs.get(1)[1]));
-			 * // Queen Queen3 = new Queen(new Position(whiteQueenLocs.get(2)[0], //
-			 * whiteQueenLocs.get(2)[1])); // Queen Queen4 = new Queen(new
-			 * Position(whiteQueenLocs.get(3)[0], // whiteQueenLocs.get(3)[1])); // get
-			 * legal moves LegalMove moveGetter = new LegalMove(); ArrayList<Position>
-			 * movesQueen1 = moveGetter.getLegalMove(Queen1, curr);
-			 * System.out.println("Getting legal moves for Queen 1, ex: " +
-			 * movesQueen1.get(0).getX() + "," + movesQueen1.get(0).getY()); //
-			 * ArrayList<Position> movesQueen2 = moveGetter.getLegalMove(Queen2, curr); //
-			 * ArrayList<Position> movesQueen3 = moveGetter.getLegalMove(Queen3, curr); //
-			 * ArrayList<Position> movesQueen4 = moveGetter.getLegalMove(Queen4, curr);
-			 * //int counter = -20; for (Position position : movesQueen1) { System.out
-			 * .println("For each move in the queen's move list generate a full list of possible arrow shots"
-			 * ); ArrayList<Position> arrowMoves = moveGetter.getLegalMove(new
-			 * Queen(position), curr); System.out.println("ex: " + arrowMoves.get(0).getX()
-			 * + "," + arrowMoves.get(0).getY());
-			 * 
-			 * for (Position position2 : arrowMoves) { //counter++; System.out.
-			 * println("For each arrow move position lets generate a board and add it to the tree"
-			 * ); ArrayList<Integer> QueenPosCur = new ArrayList<>(); ArrayList<Integer>
-			 * QueenPosNew = new ArrayList<>(); ArrayList<Integer> ArrowPos = new
-			 * ArrayList<>();
-			 * 
-			 * QueenPosCur.add(Queen1.getQueenCurrX());
-			 * QueenPosCur.add(Queen1.getQueenCurrY());
-			 * 
-			 * QueenPosNew.add(position.getX()); QueenPosNew.add(position.getY());
-			 * 
-			 * ArrowPos.add(position2.getX()); ArrowPos.add(position2.getY());
-			 * 
-			 * // clone the current board so we can add a updated version to the tree that
-			 * // doesn't affect our current board
-			 * System.out.println("curr before clone: "); curr.printBoard();
-			 * System.out.println("tempbaord after clone but before update: "); Board
-			 * tempBoard = (Board) curr.clone(); tempBoard.printBoard();
-			 * 
-			 * System.out.println("tempbaord after update");
-			 * tempBoard.updateGameBoard(tempBoard, QueenPosCur, QueenPosNew, ArrowPos,
-			 * false); tempBoard.printBoard();
-			 * 
-			 * System.out.println("Cur after updating tempboard (should be unchanged))");
-			 * curr.printBoard(); //System.out.
-			 * println("Find out what part is not cloning:\nBoards: currBoard- " +
-			 * curr.board.toString() + "\ntempBaord- " + tempBoard.board.toString() +
-			 * "\nWhite Pos curr- " + curr.WhitePos + "\n Temp whitepos- " +
-			 * tempBoard.WhitePos + "\n Cur black pos- " + curr.BlackPos +
-			 * "\n Temp Blackpos- " + tempBoard.BlackPos ); Node tempNode = new
-			 * Node(this.root, 0); tempNode.setBoard(tempBoard);
-			 * partial.root.addChild(tempNode);
-			 * 
-			 * //if(counter==1) //break;
-			 * 
-			 * } //counter++; System.out.println("tree generation done"); //if(counter==2)
-			 * //break; }
-			 */
 		} else { // generate moves for black
-
-			// get queens
-			TerritoryHeuristic getQueens = new TerritoryHeuristic();
-			ArrayList<int[]> blackQueenLocs = getQueens.BlackQueenLocations(curr.board);
-			Queen Queen1 = new Queen(new Position(blackQueenLocs.get(0)[0], blackQueenLocs.get(0)[1]));
-			// Queen Queen2 = new Queen(new Position(blackQueenLocs.get(1)[0],
-			// blackQueenLocs.get(1)[1]));
-			// Queen Queen3 = new Queen(new Position(blackQueenLocs.get(2)[0],
-			// blackQueenLocs.get(2)[1]));
-			// Queen Queen4 = new Queen(new Position(blackQueenLocs.get(3)[0],
-			// blackQueenLocs.get(3)[1]));
-
-			// get legal moves for queens
-			LegalMove moveGetter = new LegalMove();
-			ArrayList<Position> movesQueen1 = moveGetter.getLegalMove(Queen1, curr);
-			// ArrayList<Position> movesQueen2 = moveGetter.getLegalMove(Queen2, curr);
-			// ArrayList<Position> movesQueen3 = moveGetter.getLegalMove(Queen3, curr);
-			// ArrayList<Position> movesQueen4 = moveGetter.getLegalMove(Queen4, curr);
-
-			// TODO - pass each possible queen position to movegetter to get possible arrow
-			// positions, then get board states from this and add them to the tree
-			// get board states for each move
-
-			for (Position queenPos : movesQueen1) {
-				ArrayList<Position> arrowMoves = moveGetter.getLegalMove(new Queen(queenPos), curr);
-
-				for (Position arrowPos : arrowMoves) {
-					ArrayList<Integer> QueenPosCur = new ArrayList<>();
-					ArrayList<Integer> QueenPosNew = new ArrayList<>();
-					ArrayList<Integer> ArrowPos = new ArrayList<>();
-
-					QueenPosCur.add(Queen1.getQueenCurrX());
-					QueenPosCur.add(Queen1.getQueenCurrY());
-
-					QueenPosNew.add(queenPos.getX());
-					QueenPosNew.add(queenPos.getY());
-
-					ArrowPos.add(arrowPos.getX());
-					ArrowPos.add(arrowPos.getY());
-
-					Board tempBoard = (Board) curr.clone();
-
-					tempBoard.updateGameBoard(curr, QueenPosCur, QueenPosNew, ArrowPos, false);
-
-					Node tempNode = new Node(this.root, 0);
-					tempNode.setBoard(tempBoard);
-					partial.root.addChild(tempNode);
+			// get queen
+			// TerritoryHeuristic getQueens = new TerritoryHeuristic();
+			// ArrayList<int[]> blackQueenLocs = getQueens.BlackQueenLocations(curr.board);
+			ArrayList<int[]> blackQueenLocs = new ArrayList<>();
+			for (int i = 0; i < curr.board.length; i++) {
+				for (int j = 0; j < curr.board.length; j++) {
+					if (curr.board[i][j] == 2) {
+						int[] temp = new int[2];
+						temp[0] = i;
+						temp[1] = j;
+						blackQueenLocs.add(temp);
+					}
 				}
 			}
 
+			for (int i = 0; i < 4; i++) {
+				Queen Queen = new Queen(new Position(blackQueenLocs.get(i)[0], blackQueenLocs.get(i)[1]));
+				/*
+				 * System.out.println("Queen1: " + whiteQueenLocs.get(0)[0] + "," +
+				 * whiteQueenLocs.get(0)[1] + "\n" + "Queen2: " + whiteQueenLocs.get(1)[0] + ","
+				 * + whiteQueenLocs.get(1)[1] + "\n" + "Queen3: " + whiteQueenLocs.get(2)[0] +
+				 * "," + whiteQueenLocs.get(2)[1] + "\n" + "Queen4: " + whiteQueenLocs.get(3)[0]
+				 * + "," + whiteQueenLocs.get(3)[1] + "\n");
+				 */
+				LegalMove moveGetter = new LegalMove();
+				ArrayList<Position> movesQueen = moveGetter.getLegalMove(Queen, curr);
+				for (Position position : movesQueen) {
+					ArrayList<Position> arrowMoves = moveGetter.getLegalMove(new Queen(position), curr);
+					for (Position position2 : arrowMoves) {
+
+						ArrayList<Integer> QueenPosCur = new ArrayList<>();
+						ArrayList<Integer> QueenPosNew = new ArrayList<>();
+						ArrayList<Integer> ArrowPos = new ArrayList<>();
+
+						QueenPosCur.add(Queen.getQueenCurrX());
+						QueenPosCur.add(Queen.getQueenCurrY());
+
+						QueenPosNew.add(position.getX());
+						QueenPosNew.add(position.getY());
+
+						ArrowPos.add(position2.getX());
+						ArrowPos.add(position2.getY());
+
+						Board tempBoard = (Board) curr.clone();
+
+						tempBoard.updateGameBoard(tempBoard, QueenPosCur, QueenPosNew, ArrowPos, false);
+
+						Node tempNode = new Node(this.root, 0);
+						tempNode.setBoard(tempBoard);
+						partial.root.addChild(tempNode);
+
+					}
+
+				}
+
+			}
 		}
 		return partial;
 	}
