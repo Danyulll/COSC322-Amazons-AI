173e602190f005f4d3a832586244ce6143feea51
Danyulll <dkrasnov@student.ubc.ca>
2022-04-01 16:35:22 -0700

    Bug fixed depth calculator

--- team-00/src/main/java/ubc/cosc322/COSC322Test.java
+++ team-00/src/main/java/ubc/cosc322/COSC322Test.java
@@ -198,11 +198,9 @@ public class COSC322Test extends GamePlayer {
 					ply=2;
 				else if(possible<=3000 && possible >2216 )
 					ply=3;
-				else if(possible<=2216 &&possible >1058 )
+				else if(possible<=2216 )
 					ply=3;
-				else if(possible<=1058 &&possible >529 )
-					ply=4;
-
+				
 				System.out.println("ply chose and possible count: ("+ply+","+possible+")");
 				Tree partial = new Tree();
 				partial.getRoot().setBoard(boardBeforeMove);
